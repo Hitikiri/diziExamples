d5bbef1d4841be4840cab264637ecdda49967db5
bekiricli <82258691+Hitikiri@users.noreply.github.com>
2022-02-19 11:31:37 +0300

    DiziExamples

---
+++ diziExample2/diziExample2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace diziExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10 elemanlı bir dizi 1-200 arasında rastgele sayılar atayın
+            //A-100den büyük sayıların sayısını bulun
+            //B-150den küçük sayıların ortalaması
+            //C-Çift sayıların sayısını bulun
+
+            int[] sayilar = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = rnd.Next(1, 200);
+            }
+            //A
+            int sayac = 0;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (sayilar[i]>100)
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine("Yüzden büyük sayıların sayısı {0}",sayac);
+            //B
+            int ortalama, toplam=0, sayacB=0;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (sayilar[i]<150)
+                {
+                    toplam += sayilar[i];
+                    sayacB++;
+                }
+            }
+
+            ortalama = toplam / sayacB;
+            Console.WriteLine("150den küçük sayıların ortalamsı:{0}",ortalama);
+        }
+    }
+}
---
+++ diziExamples/diziExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace diziExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Bir dizideki en küçük ve en büyük sayıları bulan kod
+
+           int[] dizi = new int[] {2, 5, 1, 6, 20, 222, 13245, 5, 6, 9};
+           int enKucuk=dizi[0],enBuyuk=dizi[0];
+           for (int i = 0; i < dizi.Length; i++)
+           {
+               if (enKucuk>dizi[i])
+               {
+                   enKucuk = dizi[i];
+                   
+               }
+
+               if (enBuyuk<dizi[i])
+               {
+                   enBuyuk = dizi[i];
+               }
+           }
+           Console.WriteLine("{0} en büyük eleman ve {1} en küçük elemandır.",enBuyuk,enKucuk);
+        }
+    }
+}
